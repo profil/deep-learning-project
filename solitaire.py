ce8f586271313574b93949b25903b6df06494f8a
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2016-05-13 03:58:26 +0200

    Initial cursor movement

--- solitaire.py
+++ solitaire.py
@@ -2,6 +2,7 @@
 import pygame
 from os import path
 from random import shuffle
+from pygame.locals import *
 
 CARDWIDTH = 73
 CARDHEIGHT = 97
@@ -49,15 +50,28 @@ class Deck:
             self.deck = self.showed
             self.showed = []
 
+    def cards_in_stack(self, stack):
+        return len(self.rows[stack])
+
+class Cursor:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
 class Solitaire:
     def __init__(self, screen, cards, backside, bottom):
         self.screen = screen
         self.cards = cards
         self.backside = backside
         self.bottom = bottom
-        self.cursor = (0, 0)
+        self.cursor = Cursor(0, 1)
         self.selected = False
         self.reset()
+
+
+    def cards_in_stack(self):
+        return self.deck.cards_in_stack(self.cursor.x)
 
     def draw(self):
         self.screen.blit(self.backside, (0, 0))
@@ -79,16 +93,42 @@ class Solitaire:
                 self.screen.blit(card, ((MARGIN + CARDWIDTH) * i, (2 * MARGIN + CARDHEIGHT) + y))
                 y += OFFSET
 
-        pygame.draw.rect(self.screen,
-                         CURSOR_COLOR,
-                         pygame.Rect((MARGIN + CARDWIDTH) * 3,
-                                     OFFSET + (2 * MARGIN + CARDHEIGHT),
-                                     CARDWIDTH,
-                                     CARDHEIGHT + 2 * OFFSET),
-                         2)
+        self.draw_cursor()
+
+    def move_right(self):
+        if self.cursor.x == 1 and self.cursor.y == 0: # == 0 for clearance
+            self.cursor.x = 3
+        else:
+            self.cursor.x = min(self.cursor.x + 1, 6)
+
+    def move_left(self):
+        if self.cursor.x == 3 and self.cursor.y == 0: # == 0 for clearance
+            self.cursor.x = 1
+        else:
+            self.cursor.x = max(self.cursor.x - 1, 0)
+
+    def move_down(self):
+        self.cursor.y = 1
+
+    def move_up(self):
+        if self.cursor.x != 2:
+            self.cursor.y = 0
 
     def reset(self):
         self.deck = Deck()
+
+    def draw_cursor(self):
+        y = self.cursor.y * (2 * MARGIN + CARDHEIGHT)
+        if self.cursor.y: # == 1
+            y += OFFSET * (self.cards_in_stack() - 1)
+
+        pygame.draw.rect(self.screen,
+                         CURSOR_COLOR,
+                         pygame.Rect((MARGIN + CARDWIDTH) * self.cursor.x,
+                         y,
+                         CARDWIDTH,
+                         CARDHEIGHT),
+                         2)
 
 def init_game():
     cards = [[pygame.image.load(path.join('cards', '{0:02d}'.format(value) + suit + ".gif"))
@@ -108,17 +148,28 @@ def init_game():
 
     screen.blit(background, (0, 0))
     pygame.display.update()
-    pygame.time.delay(1000)
 
-    for i in range(2):
-        background.fill((0, 130, 0))
+    while 1:
+        event = pygame.event.wait()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                solitaire.move_left()
+            elif event.key == pygame.K_RIGHT:
+                solitaire.move_right()
+            elif event.key == pygame.K_UP:
+                solitaire.move_up()
+            elif event.key == pygame.K_DOWN:
+                solitaire.move_down()
+            background.fill((0, 130, 0))
 
-        solitaire.deck.deal()
-        solitaire.draw()
+            solitaire.deck.deal()
+            solitaire.draw()
 
-        screen.blit(background, (0, 0))
-        pygame.display.update()
-        pygame.time.delay(2000)
+            screen.blit(background, (0, 0))
+            pygame.display.update()
+        elif event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
 
 def main():
     init_game()
